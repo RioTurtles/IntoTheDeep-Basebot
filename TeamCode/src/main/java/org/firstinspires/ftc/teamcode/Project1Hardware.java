efedcb550f8fbd82cb620193c9aac13aea75093b
Airblox <106415728+Airblox@users.noreply.github.com>
2024-10-09 16:41:25 +0800

    Hardware map & TeleOp draft

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Project1Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Project1Hardware.java
@@ -1,23 +1,46 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Project1Hardware {
     DcMotor motorFL, motorFR;
     DcMotor motorBL, motorBR;
+    DcMotorEx sliderL, sliderR;
+    ServoImplEx claw;
+    ServoImplEx armL, armR;
+    MecanumDrive drivetrain;
     IMU imu;
 
-    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+    int mode, height;
+    boolean clawOpen, armUp;
+
+    public Project1Hardware(HardwareMap hardwareMap) {
+        init(hardwareMap);
+        reset();
+        clawOpen = false;
+        armUp = false;
+    }
+
+    private void init(@NonNull HardwareMap hardwareMap) {
         motorFL = hardwareMap.get(DcMotor.class, "motorFL");
         motorFR = hardwareMap.get(DcMotor.class, "motorFR");
         motorBL = hardwareMap.get(DcMotor.class, "motorBL");
         motorBR = hardwareMap.get(DcMotor.class, "motorBR");
+        sliderL = hardwareMap.get(DcMotorEx.class, "sliderL");
+        sliderR = hardwareMap.get(DcMotorEx.class, "sliderR");
+        claw = hardwareMap.get(ServoImplEx.class, "claw");
+        armL = hardwareMap.get(ServoImplEx.class, "armL");
+        armR = hardwareMap.get(ServoImplEx.class, "armR");
         imu = hardwareMap.get(IMU.class, "imu");
 
         //TODO: Change directions
@@ -25,9 +48,11 @@ public class Project1Hardware {
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
         )));
+
+        drivetrain = new MecanumDrive();
     }
 
-    public void reset() {
+    private void reset() {
         motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -43,9 +68,232 @@ public class Project1Hardware {
         motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        sliderL.setDirection(DcMotorSimple.Direction.FORWARD);
+        sliderR.setDirection(DcMotorSimple.Direction.REVERSE);
+        sliderL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        sliderR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
         )));
+    }
+
+    public class MecanumDrive {
+        Project1Hardware robot;
+        double max, sin, cos, theta, power, vertical, horizontal, pivot, heading;
+        double FLPower, FRPower, BLPower, BRPower;
+
+        public MecanumDrive() {this.robot = Project1Hardware.this;}
+
+        /**
+         * Classic drivetrain movement method - self explanatory.
+         * @param vertical Gamepad's vertical axis (y).
+         * @param horizontal Gamepad's horizontal axis (x).
+         * @param pivot Gamepad's rotational axis (<code>right_stick_x</code>).
+         * @param heading Robot's heading.
+         */
+        public void remote(double vertical, double horizontal, double pivot, double heading) {
+            this.vertical = vertical;
+            this.horizontal = horizontal;
+            this.pivot = pivot;
+            this.heading = heading ;
+
+            theta = 2 * Math.PI + Math.atan2(vertical,horizontal) - heading;
+            power = Math.hypot(horizontal, vertical);
+
+            sin = Math.sin(theta - Math.PI/4);
+            cos = Math.cos(theta - Math.PI/4);
+            max = Math.max(Math.abs(sin), Math.abs(cos));
+
+            FLPower = power * (cos/max) + pivot;
+            FRPower = power * sin/max - pivot;
+            BLPower = power * -(sin/max) - pivot;
+            BRPower = power * -(cos/max) + pivot;
+
+            robot.motorFL.setPower(-FLPower);
+            robot.motorFR.setPower(-FRPower);
+            robot.motorBL.setPower(BLPower);
+            robot.motorBR.setPower(BRPower);
+
+            robot.motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        /** Also mecanum drive ({@link #remote(double, double, double, double) remote()}) but more
+         * organised.
+         * @param vertical Gamepad's vertical axis (y).
+         * @param horizontal Gamepad's horizontal axis (x).
+         * @param pivot Gamepad's rotational axis (<code>right_stick_x</code>).
+         * @param heading Robot's heading.
+         */
+        public void remote2(double vertical, double horizontal, double pivot, double heading) {
+            robot.motorFL.setDirection(DcMotor.Direction.FORWARD);
+            robot.motorFR.setDirection(DcMotor.Direction.REVERSE);
+            robot.motorBL.setDirection(DcMotor.Direction.FORWARD);
+            robot.motorBR.setDirection(DcMotor.Direction.REVERSE);
+
+            this.vertical = vertical;
+            this.horizontal = horizontal;
+            this.pivot = pivot;
+            this.heading = heading + (Math.PI/2);
+
+            theta = 2 * Math.PI + Math.atan2(vertical, horizontal) - heading;
+            power = Math.hypot(horizontal, vertical);
+
+            sin = Math.sin(theta - Math.PI/4);
+            cos = Math.cos(theta - Math.PI/4);
+            max = Math.max(Math.abs(sin), Math.abs(cos));
+
+            /*
+                FLPower = power * (cos/max) + pivot;
+                FRPower = power * (sin/max) - pivot;
+                BLPower = power * (sin/max) + pivot;
+                BRPower = power * (cos/max) - pivot;
+            */
+
+            FLPower = power * (cos/max) - pivot;
+            FRPower = power * (sin/max) + pivot;
+            BLPower = power * (sin/max) - pivot;
+            BRPower = power * (cos/max) + pivot;
+
+            robot.motorFL.setPower(FLPower);
+            robot.motorFR.setPower(FRPower);
+            robot.motorBL.setPower(BLPower);
+            robot.motorBR.setPower(BRPower);
+
+            robot.motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        /** Undocumented - copied from MecanumDrive.java */
+        public void part1(double theta, double pivot, double power) {
+            theta = 2 * Math.PI + (theta / 360 * 2 * Math.PI) - Math.PI / 2;
+
+            sin = Math.sin(theta - Math.PI/4);
+            cos = Math.cos(theta - Math.PI/4);
+            max = Math.max(Math.abs(sin), Math.abs(cos));
+
+            FLPower = power * (cos/max) + pivot;
+            FRPower = power * sin/max - pivot;
+            BLPower = power * -(sin/max) - pivot;
+            BRPower = power * -(cos/max) + pivot;
+
+            robot.motorFL.setPower(-FLPower);
+            robot.motorFR.setPower(-FRPower);
+            robot.motorBL.setPower(BLPower);
+            robot.motorBR.setPower(BRPower);
+
+            robot.motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            robot.motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        /** Undocumented - copied from MecanumDrive.java */
+        public void drive(double target, double power, double pivot, double distance) {
+
+            this.theta = Math.PI + (target * Math.PI/180);
+            sin = Math.sin(theta - Math.PI/4);
+            cos = Math.cos(theta - Math.PI/4);
+            max = Math.max(Math.abs(sin), Math.abs(cos));
+
+            int FL = robot.motorFL.getCurrentPosition();
+            int FR = robot.motorFR.getCurrentPosition();
+            int BL = robot.motorBL.getCurrentPosition();
+            int BR = robot.motorBR.getCurrentPosition();
+
+            double orig = FL;
+            double cur = orig;
+
+            while (Math.abs(cur - orig) <= distance) {
+                FL = robot.motorFL.getCurrentPosition();
+                FR = robot.motorFR.getCurrentPosition();
+                BL = robot.motorBL.getCurrentPosition();
+                BR = robot.motorBR.getCurrentPosition();
+
+                cur = FL;
+
+                FLPower = power * -(cos/max) + pivot;
+                FRPower = power * sin/max + pivot;
+                BLPower = power * -(sin/max) + pivot;
+                BRPower = power * cos/max + pivot;
+
+                robot.motorFL.setPower(-FLPower);
+                robot.motorFR.setPower(-FRPower);
+                robot.motorBL.setPower(BLPower);
+                robot.motorBR.setPower(BRPower);
+
+                robot.motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                robot.motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
+        }
+    }
+
+    // Claw tuned
+    public void clawOpen() {claw.setPosition(0.2); clawOpen = true;}
+    public void clawClose() {claw.setPosition(0); clawOpen = false;}
+
+    public void armUp() {
+        armL.setPosition(0.25);
+        armR.setPosition(0.25);
+        armUp = false;
+    }
+
+    public void armDown() {
+        armL.setPosition(0.1);
+        armR.setPosition(0.1);
+        armUp = false;
+    }
+
+    public void setSliderPosition(int mode, int height) {
+        switch (height) {
+            case 0:
+                sliderL.setTargetPosition(0);
+                sliderR.setTargetPosition(0);
+                break;
+            case 1:
+                switch (mode) {
+                    case 1:
+                        sliderL.setTargetPosition(400);
+                        sliderR.setTargetPosition(400);
+                        break;
+                    case 2:
+                        sliderL.setTargetPosition(300);
+                        sliderR.setTargetPosition(300);
+                        break;
+                }
+                break;
+
+            case 2:
+                switch (mode) {
+                    case 1:
+                        sliderL.setTargetPosition(700);
+                        sliderR.setTargetPosition(700);
+                        break;
+                    case 2:
+                        sliderL.setTargetPosition(800);
+                        sliderR.setTargetPosition(800);
+                        break;
+                }
+                break;
+        }
+
+        sliderL.setPower(1);
+        sliderR.setPower(1);
+        sliderL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        sliderR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void setSliderPosition() {setSliderPosition(this.mode, this.height);}
+
+    public boolean isSliderInPosition() {
+        return Math.abs(sliderL.getCurrentPosition() - sliderL.getTargetPosition()) <= 5;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_v1.java
@@ -2,33 +2,113 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class Teleop_v1 extends LinearOpMode {
-    double direction_y, direction_x, pivot, heading;
+    Project1Hardware robot;
+    States state;
+    Gamepad gamepad;
+    Gamepad lastGamepad;
+    double direction_x, direction_y, pivot, heading;
+
+    ElapsedTime timer1;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Project1Hardware robot = new Project1Hardware();
-        MecanumDrive drivetrain = new MecanumDrive(robot);
-        robot.init(hardwareMap, telemetry);
-        robot.reset();
+        robot = new Project1Hardware(hardwareMap);
+        state = States.INTAKE_READY;
+        gamepad = new Gamepad();
+        lastGamepad = new Gamepad();
+        timer1 = new ElapsedTime();
 
         waitForStart();
         robot.imu.resetYaw();
-        drivetrain.remote(0,0,0,0);
+        timer1.reset();
         while (opModeIsActive()) {
-            direction_y = gamepad1.left_stick_y;
+            lastGamepad.copy(gamepad);
+            gamepad.copy(gamepad1);
             direction_x = -gamepad1.left_stick_x;
+            direction_y = gamepad1.left_stick_y;
             pivot = gamepad1.right_stick_x * 0.8;
             heading = robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            if (gamepad1.touchpad) robot.imu.resetYaw();
+            if (gamepad.touchpad) robot.imu.resetYaw();
 
+            if (state == States.INTAKE_READY) {
+                if (timer1.milliseconds() > 300) robot.clawOpen();
+
+                if (gamepad.left_bumper && !lastGamepad.left_bumper) {
+                    if (robot.clawOpen) robot.clawClose(); else robot.clawOpen();
+                }
+
+                if (gamepad.right_bumper && !lastGamepad.right_bumper) {
+                    timer1.reset();
+                    state = States.TRANSFER_ONE;
+                }
+            }
+
+            if (state == States.TRANSFER_ONE) {
+                robot.clawClose();
+                if (timer1.milliseconds() > 300) robot.armUp();
+
+                if (gamepad.left_bumper && !lastGamepad.left_bumper) {
+                    robot.armDown();
+                    state = States.INTAKE_READY;
+                    timer1.reset();
+                }
+
+                if (gamepad.right_bumper && !lastGamepad.right_bumper) {
+                    state = States.SCORING_READY;
+                }
+            }
+
+            if (state == States.SCORING_READY) {
+                robot.setSliderPosition();
+                if (robot.isSliderInPosition()) {
+                    if (gamepad.right_trigger > 0 && !(lastGamepad.right_trigger > 0)) {
+                        if (robot.clawOpen) robot.clawClose(); else robot.clawOpen();
+                    }
+                }
+
+                if (gamepad.left_bumper && !lastGamepad.left_bumper) {
+                    robot.setSliderPosition(1, 0);
+                    state = States.TRANSFER_TWO;
+                    timer1.reset();
+                }
+            }
+
+            if (state == States.TRANSFER_TWO) {
+                robot.setSliderPosition(1, 0);
+                robot.clawOpen();
+                if (robot.isSliderInPosition()) state = States.INTAKE_READY;
+
+                if (gamepad.left_bumper && !gamepad.left_bumper) {
+                    state = States.SCORING_READY;
+                    timer1.reset();
+                }
+            }
+
+            if (gamepad.triangle) robot.mode = 1;
+            if (gamepad.circle) robot.mode = 2;
+            if (gamepad.cross) robot.height = 1;
+            if (gamepad.square) robot.height = 2;
+
+            telemetry.addData("State", state);
+            telemetry.addData("Mode", robot.mode);
+            telemetry.addData("Height", robot.height);
             telemetry.update();
-            drivetrain.remote(direction_y, -pivot, direction_x, heading);
+            robot.drivetrain.remote(direction_y, -pivot, direction_x, heading);
         }
+    }
+
+    enum States {
+        INTAKE_READY,
+        TRANSFER_ONE,
+        SCORING_READY,
+        TRANSFER_TWO
     }
 }
